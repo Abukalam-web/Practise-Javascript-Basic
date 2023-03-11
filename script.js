4e55855246458f619500304d418ad0bd053f0218
ABU KALAM <99273096+Abukalam-web@users.noreply.github.com>
2023-03-11 07:12:45 +0600

    gfjkgjfg

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function showTime(){
     s = (s < 10) ? "0" + s : s;
    
     
-    var time = h + ":" + m + ":" + s + ":"  + session;
+    var time = h + ":" + m + ":" + s + ":"   + session;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     
